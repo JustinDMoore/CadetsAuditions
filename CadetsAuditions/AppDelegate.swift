68182d8fc57839db9ed409212bf69f7435fd1144
Justin Moore <JMoore@JustinMoore-MacbookPro.local>
2016-11-09 21:41:35 -0500

    Initial Commit

---
+++ CadetsAuditions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CadetsAuditions
+//
+//  Created by Justin Moore on 11/9/16.
+//  Copyright © 2016 Justin Moore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CadetsAuditions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CadetsAuditions
+//
+//  Created by Justin Moore on 11/9/16.
+//  Copyright © 2016 Justin Moore. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
